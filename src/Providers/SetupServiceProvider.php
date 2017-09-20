bc76880cbfa7983556b1d629f56dda20ecf35e17
Rahul makhija <rahulmkhj@gmail.com>
2017-09-20 18:20:40 +0530

    updated SetupServiceProvider
    added newly created commands, and removed registration for package
    routes & views.

--- src/Providers/SetupServiceProvider.php
+++ src/Providers/SetupServiceProvider.php
@@ -3,13 +3,33 @@
 namespace AccessManager\Setup\Providers;
 
 
+use AccessManager\Setup\Commands\ConfigDatabaseCommand;
+use AccessManager\Setup\Commands\FreshAssetsCommand;
+use AccessManager\Setup\Commands\FreshSetupCommand;
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * Class SetupServiceProvider
+ * @package AccessManager\Setup
+ */
 class SetupServiceProvider extends ServiceProvider
 {
+    /**
+     * Define artisan commands to be registered.
+     *
+     * @var array
+     */
+    protected $commands = [
+        FreshSetupCommand::class,
+        FreshAssetsCommand::class,
+        ConfigDatabaseCommand::class,
+    ];
+
+    /**
+     * Register defined artisan commands.
+     */
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . "/../Routes/web.php");
-        $this->loadViewsFrom( __DIR__ . "/../Views", "Setup");
+        $this->commands( $this->commands );
     }
 }
