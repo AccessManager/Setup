59e785953905ec2108ef6b903629c245982b8d05
Rahul makhija <rahulmkhj@gmail.com>
2017-09-23 08:42:40 +0530

    update ConfigDatabaseCommand
    modified class to not use jackiedo/DotenvEditor, and utilize newly
    custom helper class to modify .env file.

--- src/Commands/ConfigDatabaseCommand.php
+++ src/Commands/ConfigDatabaseCommand.php
@@ -3,9 +3,9 @@
 namespace AccessManager\Setup\Commands;
 
 
-use Illuminate\Config\Repository;
+use AccessManager\Helpers\DotEnvEditor;
 use Illuminate\Console\Command;
-use Jackiedo\DotenvEditor\DotenvEditor;
+use Illuminate\Support\Facades\DB;
 
 /**
  * Class ConfigDatabaseCommand
@@ -24,16 +24,6 @@ class ConfigDatabaseCommand extends Command
     protected $description = "configure or update database parameters.";
 
     /**
-     * @var DotenvEditor
-     */
-    protected $env;
-
-    /**
-     * @var Repository
-     */
-    protected $config;
-
-    /**
      * Takes parameters from user and update the .env file with new values.
      */
     public function handle()
@@ -44,65 +34,15 @@ class ConfigDatabaseCommand extends Command
         $dbUser = $this->ask("Database Username", 'root');
         $dbPass = $this->ask("Database Password", 'root');
 
-        $this->writeEnvironmentVariablesToEnv([
-            'APP_NAME'      =>  'Access Manager',
-            'DB_HOST'       =>  $dbHost,
-            'DB_DATABASE'   =>  $dbName,
-            'DB_USERNAME'   =>  $dbUser,
-            'DB_PASSWORD'   =>  $dbPass,
-        ]);
+        $envEditor = new DotEnvEditor($this->laravel->environmentFilePath());
 
-        $this->setEnvironmentVariables([
-            'host'      =>  $dbHost,
-            'database'  =>  $dbName,
-            'username'  =>  $dbUser,
-            'password'  =>  $dbPass,
-        ]);
+        $envEditor->setDbHost($dbHost)
+            ->setDbDatabase($dbName)
+            ->setDbUsername($dbUser)
+            ->setDbPassword($dbPass)
+            ->save();
+        DB::purge('mysql');
+        $this->info("Configuration Successfully Updated.");
     }
-
-    /**
-     * Updates .env file with new variable values.
-     * @param array $params
-     */
-    protected function writeEnvironmentVariablesToEnv( array $params )
-    {
-        $variables = [];
-        foreach ($params as $key => $value )
-        {
-            $variables[] = [
-                'key'       =>  $key,
-                'value'     =>  $value,
-                'comment'   =>  'set via access-manager config:db command',
-            ];
-        }
-        $this->env->setKeys($variables);
-        $this->env->save();
-    }
-
-    /**
-     * update current request with new config values.
-     * @param array $params
-     */
-    protected function setEnvironmentVariables( array $params )
-    {
-        foreach ( $params as $key => $value )
-        {
-            $this->config->set("database.connections.mysql.$key", $value);
-//            config(['database.connections.mysql.'. $key => $value]);
-        }
-    }
-
-    /**
-     * ConfigDatabaseCommand constructor.
-     * @param DotenvEditor $env
-     * @param Repository $config
-     */
-    public function __construct( DotenvEditor $env, Repository $config  )
-    {
-        parent::__construct();
-        $this->env = $env;
-        $this->config = $config;
-    }
-
 
 }
